164934025291e2dac32c2d294493bcd3fc9ec58f
ValentinCab <valenbunny1347@gmail.com>
2026-08-15 00:15:38 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,52 +1,48 @@
-const CACHE_NAME = "mi-glosario-v2";
+const CACHE_NAME = 'mi-glosario-v4';
 
 const APP_FILES = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icon-192.png",
-  "./icon-512.png"
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(APP_FILES))
+      .then(cache => cache.addAll(APP_FILES))
       .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key.startsWith("mi-glosario-") && key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(
+      keys
+        .filter(key => key.startsWith('mi-glosario-') && key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
 
   event.respondWith(
-    caches.match(event.request).then((cached) => {
+    caches.match(event.request).then(cached => {
       if (cached) return cached;
 
       return fetch(event.request)
-        .then((response) => {
+        .then(response => {
           const url = new URL(event.request.url);
-
           if (url.origin === self.location.origin) {
             const copy = response.clone();
-            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+            caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
           }
-
           return response;
         })
-        .catch(() => caches.match("./index.html"));
+        .catch(() => caches.match('./index.html'));
     })
   );
 });
